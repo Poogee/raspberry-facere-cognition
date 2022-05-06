4cc07e13667f1a05bd3a905ccf1cd3c47183ec0f
shevlfs <shevl.fs@gmail.com>
2022-05-06 16:56:41 +0300

    multithread first iteration

--- pythonopencv/main_video.py
+++ pythonopencv/main_video.py
@@ -1,35 +1,57 @@
 import cv2
 from simple_facerec import SimpleFacerec
 import cmake
+import time
+from threading import Thread
 
 import numpy as np
 # Encode faces from a folder
-sfr = SimpleFacerec()
-sfr.load_encoding_images("images/")
-
-# Load Camera
-cap = cv2.VideoCapture(0)
 
 
-while True:
-    ret, frame = cap.read()
+class VideoStreamWidget(object):
+    def __init__(self, src=0):
+        self.capture = cv2.VideoCapture(src)
+        # Start the thread to read frames from the video stream
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
 
-    # Detect Faces
-    face_locations, face_names = sfr.detect_known_faces(frame)
-    for face_loc, name in zip(face_locations, face_names):
-        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+    def update(self):
+        # Read the next frame from the stream in a different thread
+        while True:
+            if self.capture.isOpened():
+                (self.status, self.frame) = self.capture.read()
+            time.sleep(.01)
 
-        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (255,255,255), 2)
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (255,255,255), 2)
-    # img = cv2.imread("images/dima utkin.jpg")
-    # Hori = np.concatenate((frame, img), axis=1)
-    # cv2.imshow('HORIZONTAL', Hori)
-    cv2.imshow("Frame", frame)
+    def show_frame(self):
+        # Display frames in main program
+        cv2.imshow('frame', self.frame)
+        face_locations, face_names = sfr.detect_known_faces(self.frame)
+        for face_loc, name in zip(face_locations, face_names):
+            y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+
+            cv2.putText(self.frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 2)
+            cv2.rectangle(self.frame, (x1, y1), (x2, y2), (255, 255, 255), 2)
+        # img = cv2.imread("images/dima utkin.jpg")
+        # Hori = np.concatenate((frame, img), axis=1)
+        # cv2.imshow('HORIZONTAL', Hori)
+        cv2.imshow("Frame", self.frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            self.capture.release()
+            cv2.destroyAllWindows()
+            exit(1)
 
 
-    key = cv2.waitKey(1)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+if __name__ == '__main__':
+    sfr = SimpleFacerec()
+    sfr.load_encoding_images("images/")
 
-cap.release()
-cv2.destroyAllWindows()
+    # Load Camera
+    cap = cv2.VideoCapture(0)
+    video_stream_widget = VideoStreamWidget()
+    while True:
+        try:
+            video_stream_widget.show_frame()
+        except AttributeError:
+            pass
